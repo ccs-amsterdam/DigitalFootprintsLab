571c2405f3aa66c398aa17ab686633fe91bac320
Kasper Welbers <kasperwelbers@gmail.com>
2021-06-28 11:08:33 +0200

    better global search input in data exploration page

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Input, Dimmer, Loader, Segment } from "semantic-ui-react";
+import { Input, Segment } from "semantic-ui-react";
 import db from "../apis/dexie";
 
 const SearchInput = ({ table, searchOn, selection, setSelection }) => {
@@ -14,13 +14,12 @@ const SearchInput = ({ table, searchOn, selection, setSelection }) => {
   }, [table, search, setSelectionStatus, searchOn, setSelection]);
 
   return (
-    <Segment>
-      <Input
-        loading={selectionStatus === "searching"}
-        value={search}
-        onChange={(e, d) => setSearch(d.value)}
-      ></Input>
-    </Segment>
+    <Input
+      loading={selectionStatus === "searching"}
+      value={search}
+      icon="search"
+      onChange={(e, d) => setSearch(d.value)}
+    ></Input>
   );
 };
 
@@ -38,9 +37,9 @@ const searchSelection = async (table, searchOn, search, setSelection, setSelecti
 
 export default React.memo(SearchInput);
 
-// just keeping this here so I wont forget how insanely usefull this is
+// just keeping this here so I wont forget how insanely usefull this is.
 // A second argument to React.memo lets you view the prev and next props
-// to see which bastard rerenders the living hell out of your
+// to see which bastard rerenders the living hell out of a component
 // export default React.memo(SearchInput, (prevprops, nextprops) => {
 //   console.log(prevprops);
 //   console.log(nextprops);
